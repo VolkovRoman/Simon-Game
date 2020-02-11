e4ed3206254699cdf5043007f0b983a3a1d5b915
Nymph Scarlet <51094884+Nymphana@users.noreply.github.com>
2020-02-11 16:17:33 +0300

    Initial logic done.

---
+++ game.js
@@ -0,0 +1,76 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var fadeSpeed = 100;
+var level = 1;
+
+var gameNotStarted = true;
+
+
+
+$("body").on("click", function() {
+  if (gameNotStarted) {
+    nextSequence();
+    gameNotStarted = false;
+  }
+});
+
+$(".btn").on("click", function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  animatePress(randomChosenColor);
+  playSound(randomChosenColor);
+
+  $("#level-title").text("Level " + level);
+  level++;
+}
+
+function checkAnswer(currentLevel) {
+
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    gameOver();
+  }
+
+}
+
+function gameOver() {
+  $("body").addClass("game-over");
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+  }, 200);
+  $("#level-title").text("Game Over. Click to try again");
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(buttonColor) {
+  $("#" + buttonColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + buttonColor).removeClass("pressed");
+  }, fadeSpeed);
+}
